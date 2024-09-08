618eb7f3663b2591a36cfa12dba1148b5a20a008
James Froggatt <james.froggatt@me.com>
2024-09-08 22:39:15 +0100

    Swap to Max for large iPhone

--- Github Playgrounds/Github PlaygroundsTests/SnapshotHelpers.swift
+++ Github Playgrounds/Github PlaygroundsTests/SnapshotHelpers.swift
@@ -17,7 +17,7 @@ func assertSnapshotOnDefaultDevices<Value: View>(
         let hostingVC = UIHostingController(rootView: view)
         assertSnapshot(of: hostingVC, as: .image(on: .iPhone13Mini(.landscape)), named: "iPhone13Mini-landscape", fileID: fileID, file: filePath, testName: testName, line: line, column: column)
         assertSnapshot(of: hostingVC, as: .image(on: .iPhone13Mini), named: "iPhone13Mini", fileID: fileID, file: filePath, testName: testName, line: line, column: column)
-        assertSnapshot(of: hostingVC, as: .image(on: .iPhone13Pro), named: "iPhone13Pro", fileID: fileID, file: filePath, testName: testName, line: line, column: column)
+        assertSnapshot(of: hostingVC, as: .image(on: .iPhone13ProMax), named: "iPhone13ProMax", fileID: fileID, file: filePath, testName: testName, line: line, column: column)
         assertSnapshot(of: hostingVC, as: .image(on: .iPadMini), named: "iPadMini", fileID: fileID, file: filePath, testName: testName, line: line, column: column)
         assertSnapshot(of: hostingVC, as: .image(on: .iPadPro12_9(.landscape)), named: "iPadPro12_9-landscape", fileID: fileID, file: filePath, testName: testName, line: line, column: column)
     }
