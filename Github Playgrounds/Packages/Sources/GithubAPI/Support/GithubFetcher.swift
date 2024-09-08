549884ce481231fd915172b73be1eb9ecd701f5e
James Froggatt <james.froggatt@me.com>
2024-09-08 22:34:51 +0100

    Move Page to its own file

--- Github Playgrounds/Packages/Sources/GithubAPI/Support/GithubFetcher.swift
+++ Github Playgrounds/Packages/Sources/GithubAPI/Support/GithubFetcher.swift
@@ -14,34 +14,6 @@ public extension DependencyValues {
     }
 }
 
-/// Represents a paginated API page.
-public struct Page {
-    fileprivate var url: URL
-    init(url: URL) {
-        self.url = url
-    }
-    
-    /// Create from a link header received via API.
-    public init?(nextInLinkHeader linkHeader: String) {
-        let regex = /<([^>]*)>; rel="next"/
-        do {
-            let urlString = try regex.firstMatch(in: linkHeader)?.output.1
-            if let urlString, let url = URL(string: String(urlString)) {
-                self.url = url
-            } else {
-                return nil
-            }
-        } catch {
-            print(error)
-            return nil
-        }
-    }
-    
-    public static func preview() -> Self {
-        Self.init(url: URL(string: "about:blank")!)
-    }
-}
-
 /// The protocol for the networking layer. This may be mocked out.
 public protocol GithubFetcher {
     /// Fetches content from an API, optionally fetching content following a given page.
